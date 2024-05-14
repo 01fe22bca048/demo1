4dc37bb31fb5797535c6548df9b55468d4ca7b29
01fe22bca048 <01fe22bca048@kletech.ac.in>
2024-05-14 10:27:46 +0530

    efrfe

---
+++ Employee.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Employee extends Student
+{
+    public Employee(String name, LocalDate dob) {
+        super(name, dob);
+        //TODO Auto-generated constructor stub
+    }
+
+    public void display()
+    {
+        System.out.println("Hello world");
+    }
+}
